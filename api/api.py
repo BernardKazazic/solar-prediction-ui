8482af487d8eae884f80b413d085a80c697c49f1
f-hruza0111 <52754841+f-hruza0111@users.noreply.github.com>
2025-06-05 18:09:09 +0200

    Update api.py

--- api/api.py
+++ api/api.py
@@ -663,4 +663,4 @@ def add_delay():
     time.sleep(0.2)
 
 if __name__ == '__main__':
-    app.run(debug=True, port=os.environ.get("PORT", "8000"))
+    app.run(debug=True, host="0.0.0.0" ,port=os.environ.get("PORT", "8000"))
