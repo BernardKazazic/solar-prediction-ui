d8b5d0646f9f5597796b8b151e47b07732628729
f-hruza0111 <52754841+f-hruza0111@users.noreply.github.com>
2025-06-23 14:11:12 +0200

    DevOps-1 Add build and deploy trigger action for frontend

--- solar-forecast/vite.config.ts
+++ solar-forecast/vite.config.ts
@@ -3,4 +3,13 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  base: '/',
+  server: {
+    host: '0.0.0.0',
+    port: 5173,
+    strictPort: true,
+    allowedHosts: [
+      'solar-frontend-solar.apps.okd.fh.zvne.fer.hr'
+    ]
+  }
 });
